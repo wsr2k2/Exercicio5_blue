031be25a5fd685ea8f7b3a80334e8210e9d1a366
WILLIAM DE SA RODRIGUES <73429259+wsr2k2@users.noreply.github.com>
2021-08-18 23:37:35 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let nota1 = parseInt(prompt("Digite um número: "));
+let nota2 = parseInt(prompt("Digite um número: "));
+let nota3 = parseInt(prompt("Digite um número: "));
+media = parseInt(nota1 + nota2 + nota3)/3;
+console.log(media.toFixed(2));
